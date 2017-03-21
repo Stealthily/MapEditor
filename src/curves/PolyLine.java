2fc2dab0c49204785a90843f2d3864ee6acec35e
Stealthily <Iamapanda63>
2017-03-21 01:27:03 +0100

    hey

--- src/curves/PolyLine.java
+++ src/curves/PolyLine.java
@@ -44,13 +44,17 @@ public class PolyLine extends Curve {
     public void setClosed(boolean closed) {
 
 	    if(backgroundPolygon)
+	    //Here we check whether this is the outlying polygon or not
         {
-            final Double alpha = 0.01;
+            final Double alpha = 0.8;
+            //This is the parameter responsible for the width of the rectangle
 
             for(Point2D n : finalArray)
             {
                 backgroundCoords = backgroundCoords + n.getX() + " " + n.getY()+ " ";
             }
+
+            //Here we collect the coordinates of the original outlying polygon into a new array that we split later on and convert to Double string.
             String[] tmpbgCoordsAr = backgroundCoords.split(" ");
             Double[] tmpbgCoordsArray = new Double[tmpbgCoordsAr.length];
 
@@ -58,8 +62,15 @@ public class PolyLine extends Curve {
             {
                 tmpbgCoordsArray[i]=Double.parseDouble(tmpbgCoordsAr[i]);
             }
+
+
             int a = tmpbgCoordsArray.length/2;
+
+            //Here a corresponds to the number of vectors we have in our figure
+
            Double[][] bgCoordsMatrix = new Double[a][4];
+
+           //This is an important matrix that has all pairs of points that are linked by a line (vectors in some sense) that we have to use later on to extract the linear function out of it
            int ref =0;
            for(int i=0; i<bgCoordsMatrix.length; i++)
            {
@@ -78,17 +89,11 @@ public class PolyLine extends Curve {
 
                ref=ref+2;
            }
-
-
-//           for(int i=0;i<bgCoordsMatrix.length;i++)
-//           {
-//               for(int j=0;j<4;j++)
-//               {
-//                   System.out.println(bgCoordsMatrix[i][j]);
-//               }
-//           }
+           //Here we basically fill the array so that each first point is the ending point of the previous line in the matrix, except for the case at the end, where the ending point is just the beginning of the first line since they are linked (closed shape)
 
             Double[] slopes = new Double[a];
+
+           //This containes each slope of each function in the polygon
 
            for(int i=0; i<a;i++)
            {
@@ -97,12 +102,17 @@ public class PolyLine extends Curve {
            }
 
            Double[] shifts = new Double[a];
+
+           //This contains all the "b"s of the linear functions
            for(int i=0; i<a;i++)
            {
                shifts[i]=bgCoordsMatrix[i][1]-slopes[i]*bgCoordsMatrix[i][0];
 //               System.out.println(shifts[i]);
            }
            Double[][] modifiedShifts = new Double[a][2];
+
+           //This contains all the b[1] and b[2] of the perpendicular functions (given by a^=1x+b[1] or [2]
+
             for(int i=0; i<a;i++)
             {
                 modifiedShifts[i][0]=(1/(slopes[i]))*(-bgCoordsMatrix[i][0])+bgCoordsMatrix[i][1];
@@ -115,14 +125,20 @@ public class PolyLine extends Curve {
 
             for(int k=0; k<a;k++)
             {
-                Double x1 =Math.round(modifiedShifts[k][0]-shifts[k]-alpha)/(slopes[k]-(1/slopes[k]));
-                Double x2 =(modifiedShifts[k][0]-shifts[k]+alpha)/(slopes[k]-(1/slopes[k]));
-                Double x3 =(modifiedShifts[k][1]-shifts[k]-alpha)/(slopes[k]-(1/slopes[k]));
-                Double x4 =(modifiedShifts[k][1]-shifts[k]+alpha)/(slopes[k]-(1/slopes[k]));
-                Double y1=slopes[k]*x1+shifts[k];
-                Double y2=slopes[k]*x2+shifts[k];
-                Double y3=slopes[k]*x3+shifts[k];
-                Double y4=slopes[k]*x4+shifts[k];
+                Double x1 =Math.round(((modifiedShifts[k][0]-shifts[k]-alpha)/(slopes[k]-(1/slopes[k])))*100d)/100d;
+//                Double x2 =(modifiedShifts[k][0]-shifts[k]+alpha)/(slopes[k]-(1/slopes[k]));
+                Double x2 =Math.round(((modifiedShifts[k][0]-shifts[k]+alpha)/(slopes[k]-(1/slopes[k])))*100d)/100d;
+//                Double x3 =(modifiedShifts[k][1]-shifts[k]-alpha)/(slopes[k]-(1/slopes[k]));
+                Double x3 =Math.round(((modifiedShifts[k][1]-shifts[k]-alpha)/(slopes[k]-(1/slopes[k])))*100d)/100d;
+//                Double x4 =(modifiedShifts[k][1]-shifts[k]+alpha)/(slopes[k]-(1/slopes[k]));
+                Double x4 =Math.round(((modifiedShifts[k][1]-shifts[k]+alpha)/(slopes[k]-(1/slopes[k])))*100d)/100d;
+                Double y1=Math.round((slopes[k]*x1+shifts[k])*100d)/100d;
+                Double y2=Math.round((slopes[k]*x2+shifts[k])*100d)/100d;
+                Double y3=Math.round((slopes[k]*x3+shifts[k])*100d)/100d;
+                Double y4=Math.round((slopes[k]*x4+shifts[k])*100d)/100d;
+                //(double)Math.round(value * 100000d) / 100000d
+
+
                 finished[k][0]=x1;
                 finished[k][1]=y1;
                 finished[k][2]=x2;
